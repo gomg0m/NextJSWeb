b5bb957a44c4131bee8e512b19b97b83a51cfd5f
KJY <kjy@kitech.re.kr>
2022-02-03 16:15:35 +0900

    getuser 수정

--- pages/api/getuser.js
+++ pages/api/getuser.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 let connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'kitech123',
+    password: 'kitech123!', //password: 'kitech123' //Notebook mysql 
     database: 'nextjsDemo'
 });
 
