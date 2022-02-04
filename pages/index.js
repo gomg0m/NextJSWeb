8aa095b7392e0a9270c500a6edd463161ae345cb
Daye <silvia1128@kitech.re.kr>
2022-02-04 17:38:33 +0900

    커밋

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import Home from './Home';
 import TEST from './Postproduction';
-=======
-//import Home from './Home';
-import TEST from '../src/component/PlanInfoPanel';
->>>>>>> ec7f63c1eadd316c84f449d5a13ca7875b3b6d1a
+
 
 function T1(){
     return(
