0488e02709da898919b603774cad7f8a6a9da508
JiminiiKim <jimini12@kitech.re.kr>
2022-02-11 11:32:42 +0900

    회원가입창 추가

--- pages/Storage.js
+++ pages/Storage.js
@@ -1,9 +1,17 @@
 import React from 'react';
-// import Header from './Header';
 import styles from '../src/css/Storage.module.css';
 import { Checkbox, FormControlLabel, Box, Button, Divider, Modal, Typography, InputLabel, MenuItem, 
   FormControl, Select, TextField, Paper, InputBase, IconButton, Input, Card, CardActionArea, CardMedia, CardContent } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import MuiAppBar from "@mui/material/AppBar";
+import { styled, useTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import MenuIcon from "@mui/icons-material/Menu";
+import Header from "../src/fix/Header";
+import Drawer from "@mui/material/Drawer";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 
 const style = {
@@ -23,9 +31,19 @@ const style = {
 
 function Storage() {
 
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-
       <Box className={styles.storagebackground1} sx={{ width: 1581, height: '100%', backgroundColor: '#F6F7FB', }} />
       <Box className={styles.storagebackground2} sx={{ width: 1496, height: 906, backgroundColor: 'white', }} />
       <Box className={styles.storagebackground3} sx={{ width: 1296, height: 63, backgroundColor: '#F8F8F8', borderTop: 2, borderBottom: 1 }} />
@@ -43,27 +61,25 @@ function Storage() {
           </Paper>
       </div>
 
-      
-
-      <Card className={styles.storagecard1} sx={{ width: 196, height: 196}}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="130"
-          image="images/card1.png"
-          alt="card1"
-        />
-        <CardContent>
-          <Typography gutterBottom className={styles.storagecardtitle} component="div">
-            기술구체화 협의 자료.jpeg
-          </Typography>
-          <Typography variant="body2" className={styles.storagecardsub}>
-            2021-10-01   110.2KB
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-
+    
+        <Card className={styles.storagecard1} sx={{ width: 196, height: 196}}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="130"
+            image="images/card1.png"
+            alt="card1"
+          />
+          <CardContent>
+            <Typography gutterBottom className={styles.storagecardtitle} component="div">
+              기술구체화 협의 자료.jpeg
+            </Typography>
+            <Typography variant="body2" className={styles.storagecardsub}>
+              2021-10-01   110.2KB
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </>
   );
 }
--- src/fix/HomeHeader.js
+++ src/fix/HomeHeader.js
@@ -1,28 +1,78 @@
 import styles from '../css/HomeHeader.module.css';
-import { Button, Divider } from '@mui/material';
 import Link from 'next/link';
+import React from 'react';
+import { Box, Button, Divider, Modal, Typography, InputLabel, MenuItem, FormControl, Select, TextField, Input, IconButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
-export default function Header() {
-    return (
-        <div className={styles.headerbox}>
-             <div className={styles.logocontainer}>
-                <div className={styles.logo} > <img src="images/kitechlogo.svg" alt="logo" width='100px'/></div>
-                <div className={styles.logotext}>첨단융합공연 지식기반정보플랫폼</div>             
-            </div>
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 500,
+  height: 600,
+  bgcolor: 'background.paper',
+  border: '1px solid #E0E0E0',
+  borderRadius: 2,
+  boxShadow: 24,
+  p: 4,
+};
 
-        <div className={styles.headercontainer}>
-          <Button className={styles.menubutton} variant='text'>소개</Button>
-          <Button className={styles.menubutton} variant='text'>공연DB</Button>
-          <Button className={styles.menubutton} variant='text'>공지사항</Button>
-          <Divider orientation="vertical" variant="middle" flexItem />
-          <Button className={styles.menubutton} variant='text'>회원가입</Button>
+export default function HomeHeader() {
 
-          <Link href='/Home2'>
-            <Button className={styles.loginbutton} variant='contained' color='primary'>로그인</Button>
-          </Link>
-        </div>
+  // Modal Open/Close
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className={styles.headerbox}>
+      <div className={styles.logocontainer}>
+        <div className={styles.logo} > <img src="images/kitechlogo.svg" alt="logo" width='100px'/></div>
+        <div className={styles.logotext}>첨단융합공연 지식기반정보플랫폼</div>             
+      </div>
+
+      <div className={styles.headercontainer}>
+        <Button className={styles.menubutton} variant='text'>소개</Button>
+        <Button className={styles.menubutton} variant='text'>공연DB</Button>
+        <Button className={styles.menubutton} variant='text'>공지사항</Button>
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <Button className={styles.menubutton} variant='text' onClick={handleOpen}>회원가입</Button>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+        >
+          <Box sx={style}>
+            <Typography className={styles.jointitle}>회원가입</Typography>
+            {/* <Typography className={styles.joinsubtitle}>첨단융합공연 지식기반정보플랫폼에 <br/> 오신 여러분을 환영합니다.</Typography> */}
+            <Divider className={styles.modaldivider} orientation="horizontal" variant="fullWidth" flexItem />
+
+            <div className={styles.joinoption1}>이름</div>
+            <TextField className={styles.info1} sx={{ minWidth: 250 }} id="outlined-basic" label="이름을 입력해주세요." variant="outlined"/>
+
+            <div className={styles.joinoption2}>소속회사</div>
+            <TextField className={styles.info2} sx={{ minWidth: 250 }} id="outlined-basic" label="소속회사를 입력해주세요." variant="outlined"/>
+
+            <div className={styles.joinoption3}>이메일</div>
+            <TextField className={styles.info3} sx={{ minWidth: 250 }} id="outlined-basic" label="이메일을 입력해주세요." variant="outlined"/>
             
+            <div className={styles.joinoption4}>비밀번호</div>
+            <TextField className={styles.info4} sx={{ minWidth: 250 }} id="outlined-basic" label="비밀번호를 입력해주세요." variant="outlined"/>
 
-        </div>
-    );
+            <Button className={styles.canclebutton} variant="inherit" onClick={handleClose}>취소하기</Button>
+            <Button className={styles.joinbutton} variant="contained">가입하기</Button>
+          </Box>
+        </Modal>
+
+        <Link href='/Home2'>
+          <Button className={styles.loginbutton} variant='contained' color='primary'>로그인</Button>
+        </Link>
+
+
+
+      </div>
+    </div>
+  );
 }
