ceeff6e62c2c716ebda96a7a308fda0925676888
KJY <kjy@kitech.re.kr>
2022-02-17 13:24:38 +0900

    *** JWT & Cookie 테스트 - cookie, jsonwebtoken 모듈 설치 ; yarn add로 - api/auth/login.js 및 루트 index.js 로 테스트 - 쿠기 생성 성공

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
-import TEST from './PlanInfoWrite';
+import Axios from 'axios';
+import React, {useState} from 'react';
 
+export default function Home() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-export default function T1(){
-    return(
-        <TEST />
+    const mainDivStyle = {
+        padding : '1em'
+    }
+
+    const formStyle = {display:'flex', flexDirection: 'column', maxWidth: '560px'};
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const credentials = {username, password};
+
+        const user = Axios.post('/api/auth/login', credentials).then((res)=> {
+            console.log(user);
+        } );
+
+        
+    }
+
+    return (
+        <div style={mainDivStyle}>
+            <form style={formStyle} onSubmit={e=>handleSubmit(e)}>
+                <label htmlFor='username'>Username</label>
+                <input type='text' name='username' id='username' onChange={e=>setUsername(e.target.value)}></input>
+                
+                <label htmlFor='password'>Password</label>
+                <input type='password' name='password' id='password' onChange={e=>setPassword(e.target.value)}></input>
+
+                <button> Log in </button>
+                
+            </form>
+        </div>
     );
-}
+};
